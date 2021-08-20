78291ed0d761cf728d94a835f0a9bb1738f02658
smartcodes-tz <lsuleiman2002@gmail.com>
2021-08-20 16:32:02 +0300

    added description for trades

--- lib/models/trade.dart
+++ lib/models/trade.dart
@@ -4,11 +4,13 @@ class Trade {
   String pair;
   String id;
   String result;
+  String description;
   DateTime dateTime;
 
   Trade(
       {required this.pair,
       required this.id,
       required this.result,
+      required this.description,
       required this.dateTime});
 }
--- lib/provider/trades.dart
+++ lib/provider/trades.dart
@@ -9,60 +9,80 @@ class Trades with ChangeNotifier {
       id: 'tr1',
       pair: 'USD/JPY',
       result: 'profit',
+      description:
+          'On this page, we are going to be using the provider package. If you are new to Flutter and you don’t have a strong reason to choose another approach (Redux, Rx, hooks, etc.), this is probably the approach you should start with. The provider package is easy to understand and it doesn’t use much code. It also uses concepts that are applicable in every other approach.',
       dateTime: DateTime.now(),
     ),
     Trade(
       id: 'tr2',
       pair: 'USD/CAD',
       result: 'profit',
+      description:
+          'On this page, we are going to be using the provider package. If you are new to Flutter and you don’t have a strong reason to choose another approach (Redux, Rx, hooks, etc.), this is probably the approach you should start with. The provider package is easy to understand and it doesn’t use much code. It also uses concepts that are applicable in every other approach.',
       dateTime: DateTime.now(),
     ),
     Trade(
       id: 'tr3',
       pair: 'GNP/USD',
       result: 'loss',
+      description:
+          'On this page, we are going to be using the provider package. If you are new to Flutter and you don’t have a strong reason to choose another approach (Redux, Rx, hooks, etc.), this is probably the approach you should start with. The provider package is easy to understand and it doesn’t use much code. It also uses concepts that are applicable in every other approach.',
       dateTime: DateTime.now(),
     ),
     Trade(
       id: 'tr4',
       pair: 'CAD/JPY',
       result: 'profit',
+      description:
+          'On this page, we are going to be using the provider package. If you are new to Flutter and you don’t have a strong reason to choose another approach (Redux, Rx, hooks, etc.), this is probably the approach you should start with. The provider package is easy to understand and it doesn’t use much code. It also uses concepts that are applicable in every other approach.',
       dateTime: DateTime.now(),
     ),
     Trade(
       id: 'tr5',
       pair: 'CHF/JPY',
       result: 'loss',
+      description:
+          'On this page, we are going to be using the provider package. If you are new to Flutter and you don’t have a strong reason to choose another approach (Redux, Rx, hooks, etc.), this is probably the approach you should start with. The provider package is easy to understand and it doesn’t use much code. It also uses concepts that are applicable in every other approach.',
       dateTime: DateTime.now(),
     ),
     Trade(
       id: 'tr6',
       pair: 'CAD/USD',
       result: 'loss',
+      description:
+          'On this page, we are going to be using the provider package. If you are new to Flutter and you don’t have a strong reason to choose another approach (Redux, Rx, hooks, etc.), this is probably the approach you should start with. The provider package is easy to understand and it doesn’t use much code. It also uses concepts that are applicable in every other approach.',
       dateTime: DateTime.now(),
     ),
     Trade(
       id: 'tr7',
       pair: 'USD/ZAR',
       result: 'profit',
+      description:
+          'On this page, we are going to be using the provider package. If you are new to Flutter and you don’t have a strong reason to choose another approach (Redux, Rx, hooks, etc.), this is probably the approach you should start with. The provider package is easy to understand and it doesn’t use much code. It also uses concepts that are applicable in every other approach.',
       dateTime: DateTime.now(),
     ),
     Trade(
       id: 'tr8',
       pair: 'NZD/USD',
       result: 'loss',
+      description:
+          'On this page, we are going to be using the provider package. If you are new to Flutter and you don’t have a strong reason to choose another approach (Redux, Rx, hooks, etc.), this is probably the approach you should start with. The provider package is easy to understand and it doesn’t use much code. It also uses concepts that are applicable in every other approach.',
       dateTime: DateTime.now(),
     ),
     Trade(
       id: 'tr9',
       pair: 'NZD/JPY',
       result: 'profit',
+      description:
+          'On this page, we are going to be using the provider package. If you are new to Flutter and you don’t have a strong reason to choose another approach (Redux, Rx, hooks, etc.), this is probably the approach you should start with. The provider package is easy to understand and it doesn’t use much code. It also uses concepts that are applicable in every other approach.',
       dateTime: DateTime.now(),
     ),
     Trade(
       id: 'tr10',
       pair: 'EUR/JPY',
       result: 'loss',
+      description:
+          'On this page, we are going to be using the provider package. If you are new to Flutter and you don’t have a strong reason to choose another approach (Redux, Rx, hooks, etc.), this is probably the approach you should start with. The provider package is easy to understand and it doesn’t use much code. It also uses concepts that are applicable in every other approach.',
       dateTime: DateTime.now(),
     ),
   ];
