086e020a74a7df355f2efcb0ed9320ff25f88da3
smartcodes-tz <lsuleiman2002@gmail.com>
2021-11-18 21:44:15 +0300

    added Drawer with sharing compatibility

--- lib/screens/homepage.dart
+++ lib/screens/homepage.dart
@@ -1,5 +1,6 @@
-import 'package:pipshub/authentication/authentication.dart';
-import 'package:pipshub/screens/authentication/login_signup.dart';
+import 'package:share_plus/share_plus.dart';
+import 'package:url_launcher/url_launcher.dart';
+
 import 'package:pipshub/screens/course/course_list.dart';
 import 'package:pipshub/screens/news/news_list.dart';
 import 'package:pipshub/screens/trades/add_trade.dart';
@@ -46,13 +47,51 @@ class _HomePageState extends State<HomePage> {
         // automaticallyImplyLeading: false,
       ),
       drawer: Drawer(
-        child: TextButton(
-          child: Text("Signout"),
-          onPressed: () async {
-            await context.read<AuthenticationService>().signOut();
-            Navigator.pushReplacement(context,
-                MaterialPageRoute(builder: (context) => LoginSignUpScreen()));
-          },
+        child: ListView(
+          padding: EdgeInsets.all(0),
+          children: [
+            Container(
+                height: MediaQuery.of(context).size.height / 4,
+                color: Colors.blue),
+            ListTile(
+              leading: Icon(Icons.share),
+              title: Text("Share with Friends"),
+              onTap: () async {
+                Share.share(
+                    'Check out PIPSHUB, an app for Forex trading that provides Trade Tracking System, Economic News Alert, together with different courses for FREE.  https://play.google.com/store/apps/details?id=com.tukesolutions.pipshub');
+              },
+            ),
+            ListTile(
+                leading: Icon(Icons.rate_review),
+                title: Text("Rate and Review"),
+                onTap: () async {
+                  var url =
+                      'https://play.google.com/store/apps/details?id=com.tukesolutions.pipshub';
+                  if (await canLaunch(url)) {
+                    await launch(url);
+                  }
+                }),
+            ListTile(
+              leading: Icon(FontAwesomeIcons.telegram),
+              title: Text("Join Our Telegram Channel"),
+              onTap: () async {
+                var url = 'https://t.me/pipshubappchannel';
+                if (await canLaunch(url)) {
+                  await launch(url);
+                }
+              },
+            ),
+            ListTile(
+              leading: Icon(FontAwesomeIcons.headset),
+              title: Text("Contact Support On Telegram"),
+              onTap: () async {
+                var url = 'https://t.me/pipshubapp';
+                if (await canLaunch(url)) {
+                  await launch(url);
+                }
+              },
+            ),
+          ],
         ),
       ),
       body: _screenOptions.elementAt(_selectedIndex),
